488ffb88fb015fada4e70795fd80c0c655ce335e
20536207 <e.20536207@gmail.com>
2024-03-06 06:18:27 +0700

    update 202403060618

---
+++ js/Dashboard/DashboardProfilSekolah.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    _ParentPageContains = "./pages/HomePagesHome.html";
+
+    $("#DashboardMainPage").dxScrollView({
+        scrollByContent: true,
+        scrollByThumb: true,
+        useNative: false,
+        showScrollbar: 'onHover',
+        height: '100%',
+        width: "100%",
+    });
+
+});
