98ee9c80948001891e95958d316458504c09867e
20536207 <e.20536207@gmail.com>
2024-03-15 02:27:27 +0700

    update 202403150227

--- master/User/User_MainPage.js
+++ master/User/User_MainPage.js
@@ -34,7 +34,7 @@ $(document).ready(function () {
               'data-context="signin" data-ux_mode="popup" data-callback="onSignIn" data-auto_prompt="false" >' +
             '</div>' +
   
-            '<div class="g_id_signin" data-type="standart" data-shape="pill" data-theme="outline" data-text="signin_with"' +
+            '<div style="width:90%;text-align: center; justify-content: center; margin:auto;" class="g_id_signin" data-type="standart" data-shape="pill" data-theme="outline" data-text="signin_with"' +
               'data-size="large" data-logo_alignment="left">' +
             '</div>' +
       "</div>"+
