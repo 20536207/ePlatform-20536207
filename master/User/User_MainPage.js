a5ffe9374aede8f6cb01dd75ed055b2673b4d9f9
20536207 <e.20536207@gmail.com>
2024-03-15 21:26:35 +0700

    update 202403152126

--- master/User/User_MainPage.js
+++ master/User/User_MainPage.js
@@ -57,7 +57,7 @@ $(document).ready(function () {
 
       "<img id='UserPict' style='object-fit:scale-down;width:125px;height:125px>"+
 
-      "<div id='UserAccount' style='margin: 5px 0 5px 0;'></div>" +
+      "<div id='UserAccount' style='margin: 5px 0 5px 0;'>user</div>" +
 
       '<div class="g_id_signin" data-type="standart" data-shape="pill" data-theme="outline" data-text="signin_with"' +
       'data-size="large" data-logo_alignment="left">' +
