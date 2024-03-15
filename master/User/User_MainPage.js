8c1990914c244b026c100efe08ed1e29de485aa5
20536207 <e.20536207@gmail.com>
2024-03-15 18:34:43 +0700

    update 202403151834

--- master/User/User_MainPage.js
+++ master/User/User_MainPage.js
@@ -1,3 +1,18 @@
+function decodeJwtResponse(token) {
+    var base64Url = token.split(".")[1];
+    var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    var jsonPayload = decodeURIComponent(
+      atob(base64)
+        .split("")
+        .map(function (c) {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+    );
+
+    return JSON.parse(jsonPayload);
+  }
+
 function onSignIn(response) {
   // _ApiGoogle = {
   //   "user": {
@@ -19,8 +34,8 @@ function onSignIn(response) {
   //   }
   // };
 
-  console.log(response.credential);
   const responsePayload = decodeJwtResponse(response.credential);
+  console.log(responsePayload);
   console.log(JSON.stringify(responsePayload));
 
   console.log("ID: " + responsePayload.sub);
