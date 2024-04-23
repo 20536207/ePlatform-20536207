d1c3ae74f17e970b645696ce264b394887b72741
20536207 <e.20536207@gmail.com>
2024-04-23 21:29:08 +0700

    update 202404232129

--- app/Orimain.js
+++ app/Orimain.js
@@ -1195,3 +1195,23 @@ function addPageForm(element, formColCount, itemData, itemContent) {
     }).dxForm("instance");
 
 }
+
+function printPages(elemenOfPage)  
+{  
+    var dataUrl = elemenOfPage.toDataURL(); //attempt to save base64 string to server using this var  
+    // document.getElementById(elemenOfPage).toDataURL(); //attempt to save base64 string to server using this var  
+    var windowContent = '<!DOCTYPE html>';
+    windowContent += '<html>'
+    windowContent += '<head><title>Print canvas</title></head>';
+    windowContent += '<body>'
+    windowContent += '<img src="' + dataUrl + '">';
+    windowContent += '</body>';
+    windowContent += '</html>';
+    var printWin = window.open('','','width=340,height=260');
+    printWin.document.open();
+    printWin.document.write(windowContent);
+    printWin.document.close();
+    printWin.focus();
+    printWin.print();
+    printWin.close();
+}
